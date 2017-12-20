6eab81e8ee5ad60752c4c97b87d8261cb54dc918
diman94 <94demon@gmail.com>
2017-12-20 10:07:37 +0300

    отправка сообщений через дев сервер

--- users/lib/queue_notice.py
+++ users/lib/queue_notice.py
@@ -3,9 +3,32 @@
 
 from kombu import Connection
 import json
+import requests
 
 
 def queue_notice(j_string, type, urgent=False):
+    return queue_notice_post(j_string, type, urgent=urgent)
+
+
+def queue_notice_post(j_string, type, urgent=False):
+    """Получает сообщение json и закидывает его POST запросом в b2c сервис
+       (это нужно, чтобы работать с другого сервера без доступа к RabbitMQ
+       потому что админа хер допросишься пробросить порты)
+       Формат сообщений:
+    {
+        "phone":"798811122233", #обязательно для sms
+        "mail":"vasya@mail.ru", #обязательно для email
+        "subj":"MDM Notice",    #обязательно для email
+        "token":"QWERTY12345"   #обязательно для push
+        "data": {}              #обязательно для push
+        "text":"You've just been erased" #текст, для всех видов, кроме пуш
+    }
+    """
+    url = 'https://api.safec.ru/s_mdm/lib/queue_notice'
+    r = requests.post(url, json={"j_string": j_string, "type": type, "urgent": urgent}, timeout=10)
+
+
+def queue_notice_rmq(j_string, type, urgent=False):
     """Получает сообщение json и закидывает его в необходимую очередь rabbitmq
         Формат сообщений:
     {
@@ -45,7 +68,6 @@ def queue_notice(j_string, type, urgent=False):
         rmq.put(j_string)
         print(('Sent: %s' % j_string))
         rmq.close()
-    return {'scs': True}
 
 #if __name__ == '__main__':
 #    msg = {
